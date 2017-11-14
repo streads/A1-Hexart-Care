0ac43c488e9200401eea6bd9dbb25011fecc774e
Thibaut <thibaut.lemmel@viacesi.fr>
2017-11-14 15:59:22 +0100

    Add project "Lecteur de fichier"
    main.c  action.c  action.h
    Une partie des fonctions a été complété

---
+++ module4/Lecteur de fichier/action.c
@@ -0,0 +1,119 @@
+int readCSV(hearBeat **HB){
+    char temp[LENGTH_LINE];
+    FILE *f = NULL;
+    f = fopen;
+    int i = 0;
+    while(fgets(temp, LENGTH_LINE, f) != NULL)
+    {
+        sscanf(temp, "%d;%d", &(hearBeat[i].timestamp), &(hearBeat[i].BPM));
+        i ++
+    }
+    return i -1;
+}
+
+
+hearBeat getMax(hearBeat **HB){
+    int i = 1;
+    int iFinal = 0;
+    while(HB[i] != NULL)
+    {
+        if(HB[iFinal] < HB[i])
+        {
+            iFinal = i;
+        }
+        i ++;
+    }
+    return iFinal;
+}
+
+
+hearBeat getMin(hearBeat **HB){
+    int i = 1;
+    int iFinal = 0;
+    while(HB[i] != NULL)
+    {
+        if(HB[iFinal] > HB[i])
+        {
+            iFinal = i;
+        }
+        i ++;
+    }
+    return iFinal;
+}
+
+
+float getAverage(hearBeat **HB){
+
+}
+
+
+void sort(hearBeat **HB, *(Cmp)){
+    //Thibault rempli avec son SUPER algorithme de tri fusion
+}
+
+void showInterface(void){
+    short entree = 0;
+    printf("Que voulez vous faire ?\n1. Afficher les données\n2. Afficher les données pour un temps particulier\n3. Afficher la moyenne de poul\n4. Afficher le nombre de pouls enregistré\n5. Afficher le maximum de poul\n6. Afficher le minimum de poul\n");
+    scanf("%c", entree);
+    switch(entree)
+    {
+        case 1:
+        {
+            printf("Vous voulez :\n1. Afficher les données dans l'ordre croissant de temps\n2. Afficher les données dans l'ordre décroissant de temps\n3. Afficher les données dans l'ordre croissant de pouls\n4. Afficher les données dans l'ordre décroissant de pouls");
+            scanf("%d", entree);
+            switch(entree)
+            {
+                case 1:
+                {
+                    //temps croissant
+                    return 0;
+                }
+                case 2:
+                {
+                    //temps décroissant
+                    return 0;
+                }
+                case 3:
+                {
+                    //poul croissant
+                    return 0;
+                }
+                case 4:
+                {
+                    //poul décroissant
+                    return 0;
+                }
+            }
+            return 0;
+        }
+        case 2:
+        {
+            //Il faut faire la partie "Rechercher et afficher les données pour un temps particulier"
+            printf("Cette partie est encore en construction");
+            return 0;
+        }
+        case 3:
+        {
+            printf("La moyenne du pouls est de : %f", getAverage(HB));
+            return 0;
+        }
+        case 4:
+        {
+            printf("Il y a %d pouls enregistrés", nbEntrées);
+            return 0;
+        }
+        case 5:
+        {
+            hearBeat max = getMax(HB);
+            printf("Le poul le plus élevé est de %d", max.BPM);
+            return 0;
+        }
+        case 6:
+        {
+            hearBeat min = getMin(HB);
+            printf("Le poul le plus bas est de %d", min.BPM)
+            return 0;
+        }
+    }
+
+}
---
+++ module4/Lecteur de fichier/action.h
@@ -0,0 +1,30 @@
+#ifndef ACTION_H_INCLUDED
+#define ACTION_H_INCLUDED
+
+#define LENGTH_LINE 19
+
+typedef struct hearBeat{
+    int timestamp;
+    int BPM;
+}hearBeat
+
+
+int readCSV(hearBeat **HB);
+
+
+hearBeat getMax(hearBeat **HB);
+
+
+void getMin(hearBeat **HB);
+
+
+float getAverage(hearBeat **HB);
+
+
+void sort(hearBeat **HB, *(Cmp));
+
+
+void showInterface(void);
+
+
+#endif // ACTION_H_INCLUDED
